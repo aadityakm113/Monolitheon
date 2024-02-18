d2ca53a586564e2cff420224a26d3d9de9213e34
aadityakm113 <aadityakm113@gmail.com>
2024-02-19 03:14:08 +0530

    landing page

---
+++ website/src/App.js
@@ -0,0 +1,11 @@
+import Header from "./Components/Header"
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ website/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+const Header = () => {
+  return (
+    <div className='bg'>
+        <div className='logo'>
+          <img src='/assets/textlogo.png'/>
+        </div>
+    </div>
+  )
+}
+
+export default Header
