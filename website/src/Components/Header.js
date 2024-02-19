f7ee46bee9d2805ebeb1e90e69b80a0c8ea90a8f
aadityakm113 <aadityakm113@gmail.com>
2024-02-19 21:30:57 +0530

    alt

--- website/src/Components/Header.js
+++ website/src/Components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='bg'>
         <div className='logo'>
-          <img src='/assets/textlogo.png'/>
+          <img src='/assets/textlogo.png' alt="background logo"/>
         </div>
     </div>
   )
