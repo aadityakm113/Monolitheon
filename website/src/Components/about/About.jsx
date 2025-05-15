8c6532e78556f94f7457c7f119b9f6c3dbde1922
aadityakm113 <aadityakm113@gmail.com>
2025-05-15 11:46:39 +0530

    added photos and about styled

--- website/src/Components/about/About.jsx
+++ website/src/Components/about/About.jsx
@@ -1,4 +1,5 @@
 import "../../App.css"
+import "./about.css"
 const About = () => {
   return (
     <div className='about_container'>
@@ -7,7 +8,7 @@ const About = () => {
       </div>
       <div className='content'>
         <p>
-          Previously known as Axial Paroxysm, Monolitheon is a  seven-piece progressive metal band hailing from Hyderabad that blends intricate musicianship, powerful melodies, and thought-provoking lyrics to create a captivating sonic experience. Drawing inspiration from a diverse range of influences, including progressive rock and heavy metal, Monolitheon offers a unique blend of technicality, complexity, and emotional depth that sets them apart in the prog metal scene. 
+          Previously known as Axial Paroxysm, Monolitheon is a  six-piece progressive metal band hailing from Hyderabad that blends intricate musicianship, powerful melodies, and thought-provoking lyrics to create a captivating sonic experience. Drawing inspiration from a diverse range of influences, including progressive rock and heavy metal, Monolitheon offers a unique blend of technicality, complexity, and emotional depth that sets them apart in the prog metal scene. 
         </p>
         <p>
         Even though this band is fairly new on the scene, the members of the band bring a lot of individual experience and skill to the band. Even with all our individual achievements with various other bands our friendship over the years has given us great chemistry which helps in our music and make our band sound really tight. We love composing our own songs and are looking to finish our first album soon. 
